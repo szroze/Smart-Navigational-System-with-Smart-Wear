98a7cb24eec2bc4f96035049030e04d8bbab1b96
szroze <74305657+szroze@users.noreply.github.com>
2023-04-11 10:49:13 +0800

    Add files via upload

---
+++ Obstacle Detection.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+#define maxDistance 100
+
+
+SoftwareSerial HC05(2, 1); //Rx, Tx
+int trigPin = 11;
+int echoPin = 10;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  HC05.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long dur, dis;  //duration and distance
+  //The reason for the usage of "long" is to avoid negative values returning.
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  //measure distance
+  dur = pulseIn(echoPin, HIGH);
+  dis = (dur/29.0)/2; //distance in cm
+  if (dis <= maxDistance) { //if an obstacle is within 1 meter
+     HC05.println(dis);
+     //to check with Serial monitor if the distance value is printed accurately
+     Serial.println(dis);
+  }
+  delay(200);
+}
+
+
+
+
